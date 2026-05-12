281438b761e0f6887500a38513615817a1e0b006
EmanueleDc <emanueledicredico@gmail.com>
2026-05-12 17:18:37 +0200

    commit del DAO per Autori

---
+++ Bookstore/dao/AuthorDAO.php
@@ -0,0 +1,56 @@
+<?php
+
+class AuthorDAO {
+
+    private $conn;
+
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    /** Tutti gli autori */
+    public function getAll() {
+        $res  = $this->conn->query("SELECT * FROM authors ORDER BY name");
+        return $res ? $res->fetch_all(MYSQLI_ASSOC) : [];
+    }
+
+    /** Autore per id */
+    public function getById($id) {
+        $stmt = $this->conn->prepare("SELECT * FROM authors WHERE id = ?");
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $row  = $stmt->get_result()->fetch_assoc();
+        $stmt->close();
+        return $row;
+    }
+
+    /** Crea autore */
+    public function create($name, $biography = '') {
+        $stmt = $this->conn->prepare(
+            "INSERT INTO authors (name, biography) VALUES (?, ?)"
+        );
+        $stmt->bind_param('ss', $name, $biography);
+        $ok = $stmt->execute();
+        $id = $ok ? $stmt->insert_id : false;
+        $stmt->close();
+        return $id;
+    }
+
+    /** Aggiorna autore */
+    public function update($id, $name, $biography) {
+        $stmt = $this->conn->prepare("UPDATE authors SET name = ?, biography = ? WHERE id = ?");
+        $stmt->bind_param('ssi', $name, $biography, $id);
+        $ok = $stmt->execute();
+        $stmt->close();
+        return $ok;
+    }
+
+    /** Elimina autore */
+    public function delete($id) {
+        $stmt = $this->conn->prepare("DELETE FROM authors WHERE id = ?");
+        $stmt->bind_param('i', $id);
+        $ok = $stmt->execute();
+        $stmt->close();
+        return $ok;
+    }
+}
